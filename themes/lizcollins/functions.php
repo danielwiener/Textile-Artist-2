8c1b9b051720c13c5283294b775db4234a9ee3a2
danielwiener <daniel@danielwiener.com>
2011-07-14 18:41:15 -0400

    changed to twenty ten templates, twentyeleven too complicated for this site and deleted all the header and background functions

--- themes/lizcollins/functions.php
+++ themes/lizcollins/functions.php
@@ -1,7 +1,38 @@
 <?php 
+/**
+ * Liz Collins functions and definitions
+ *
+ * Sets up the theme and provides some helper functions. Some helper functions
+ * are used in the theme as custom template tags. Others are attached to action and
+ * filter hooks in WordPress to change core functionality.
+ *
+ * The first function, twentyten_setup(), sets up the theme by registering support
+ * for various features in WordPress, such as post thumbnails, navigation menus, and the like.
+ *
+ * Functions that are not pluggable (not wrapped in function_exists()) are instead attached
+ * to a filter or action hook. The hook can be removed by using remove_action() or
+ * remove_filter() and you can attach your own function to the hook.
+ *
+ * We can remove the parent theme's hook only after it is attached, which means we need to
+ * wait until setting up the child theme:
+ *
+ * <code>
+ * add_action( 'after_setup_theme', 'my_child_theme_setup' );
+ * function my_child_theme_setup() {
+ *     // We are providing our own filter for excerpt_length (or using the unfiltered value)
+ *     remove_filter( 'excerpt_length', 'twentyten_excerpt_length' );
+ *     ...
+ * }
+ * </code>
+ *
+ *
+ * @package WordPress
+ * @subpackage Liz Collins
+ * @since Liz Collins 1.0
+ */
 
 add_action('wp_before_admin_bar_render', 'report_environment_status');
-
+ //need to fix this.need to think about changing to .com and staging etc.
 function report_environment_status() {
     $server = php_uname('n');
  	if (WP_ENV == 'development') {
@@ -14,6 +45,67 @@ function report_environment_status() {
 	'id' => 'is_development',
 	'title' => $dw_msg,
 	'href' => FALSE ) );  
-}
+} 
 
- ?> 
+
+add_action( 'after_setup_theme', 'twentyten_setup' );
+
+if ( ! function_exists( 'twentyten_setup' ) ):
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ *
+ * Note that this function is hooked into the after_setup_theme hook, which runs
+ * before the init hook. The init hook is too late for some features, such as indicating
+ * support post thumbnails.
+ *
+ * I am overriding twentyten_setup() in this child theme.
+ *
+ * @uses add_theme_support() To add support for post thumbnails and automatic feed links.
+ * @uses register_nav_menus() To add support for navigation menus.
+ * @uses add_editor_style() To style the visual editor.
+ * @uses load_theme_textdomain() For translation/localization support.
+ * @uses set_post_thumbnail_size() To set a custom post thumbnail size.
+ *
+ * @since Liz Collins 1.0
+ */
+function twentyten_setup() {
+
+	// This theme styles the visual editor with editor-style.css to match the theme style.
+	add_editor_style();
+
+	// Post Format support. You can also use the legacy "gallery" or "asides" (note the plural) categories.
+	add_theme_support( 'post-formats', array( 'aside', 'link', 'gallery', 'status', 'quote', 'image', 'video' ) );
+
+	// This theme uses post thumbnails
+	add_theme_support( 'post-thumbnails' );
+
+	// Add default posts and comments RSS feed links to head
+	add_theme_support( 'automatic-feed-links' );
+
+	// Make theme available for translation
+	// Translations can be filed in the /languages/ directory
+	load_theme_textdomain( 'twentyten', TEMPLATEPATH . '/languages' );
+
+	$locale = get_locale();
+	$locale_file = TEMPLATEPATH . "/languages/$locale.php";
+	if ( is_readable( $locale_file ) )
+		require_once( $locale_file );
+
+	// This theme uses wp_nav_menu() in one location.
+	register_nav_menus( array(
+		'primary' => __( 'Primary Navigation', 'twentyten' ),
+	) );
+
+	//I will change this as needed and add more sizes
+	set_post_thumbnail_size( 150, 150, true );
+
+	
+}
+endif;
+
+if ( ! function_exists( 'twentyten_admin_header_style' ) ) :
+function twentyten_admin_header_style() {
+// do nothing
+}
+endif;
+?> 
