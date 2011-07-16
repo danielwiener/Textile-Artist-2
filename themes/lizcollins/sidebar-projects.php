fc9bde358b6e045b606cc8a9db2ccd7d2499f9b3
danielwiener <daniel@danielwiener.com>
2011-07-16 12:44:11 -0400

    using get_posts to list the projects in the sidebar

--- themes/lizcollins/sidebar-projects.php
+++ themes/lizcollins/sidebar-projects.php
@@ -3,8 +3,8 @@
  * The Sidebar containing the primary and secondary widget areas.
  *
  * @package WordPress
- * @subpackage Twenty_Ten
- * @since Twenty Ten 1.0
+ * @subpackage Liz Collins
+ * @since Liz Collins 1.0
  */
 ?>
 
@@ -17,29 +17,23 @@
 	 * then the sidebar simply doesn't exist, so we'll hard-code in
 	 * some default sidebar stuff just in case.
 	 */
-	if ( ! dynamic_sidebar( 'primary-widget-area' ) ) : ?>
+?>          
 
-			<li id="search" class="widget-container widget_search">
-				<?php get_search_form(); ?>
-			</li>
-
-			<li id="archives" class="widget-container">
-				<h3 class="widget-title"><?php _e( 'Archives', 'twentyten' ); ?></h3>
-				<ul>
-					<?php wp_get_archives( 'type=monthly' ); ?>
-				</ul>
-			</li>
-
-			<li id="meta" class="widget-container">
-				<h3 class="widget-title"><?php _e( 'Meta', 'twentyten' ); ?></h3>
-				<ul>
-					<?php wp_register(); ?>
-					<li><?php wp_loginout(); ?></li>
-					<?php wp_meta(); ?>
-				</ul>
-			</li>
-
-		<?php endif; // end primary widget area ?>
+		<h3 class="widget-title">Projects</h3>
+			<?php
+			$args = array(
+					'post_type' => 'page', 
+					'post_status' => 'publish',
+					'post_parent' => 24,  //this will change in the remote version
+					);
+				global $post;
+				$program_pages = get_posts($args);
+				foreach($program_pages as $post) :
+				   setup_postdata($post);
+				 ?>
+				    <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+				 <?php endforeach; ?>
+			
 			</ul>
 		</div><!-- #primary .widget-area -->
 
