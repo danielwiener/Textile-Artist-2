d4b71d20e51e39d7d6c93e5f68c6933ddbdadb0a
danielwiener <daniel@danielwiener.com>
2011-07-18 12:58:52 -0400

    formatting images, first I am calling the images with  get_children(), then I am displaying them with a foreach

--- themes/lizcollins/content-page.php
+++ themes/lizcollins/content-page.php
@@ -7,7 +7,35 @@
  * @since Liz Collins 1.0
  */
 ?>
+<div id="image_grid">
+<?php   
+		$image = '';
+		$args = array(
+			'post_parent' => $post->ID,
+			'post_type' => 'attachment',
+			'post_mime_type' => 'image',
+			'orderby' => 'menu_order',
+			'order' => 'ASC',
+			'numberposts' => 16,
+			'post_status' => 'inherit'
+		);
+		$images = get_children( $args );
+	if ( $images ) :
+		//$total_images = count( $images );
+	    //$image = array_shift( $images );
+		?>
+		<ul>
+		<?php foreach ($images as $image): ?>
+		<?php $image_img_tag = wp_get_attachment_image( $image->ID, 'thumbnail' );  
+		$img_url = wp_get_attachment_url($image->ID); // url of the full size image.
+		?>
+		<li><a href="<?php echo $img_url; ?>"><?php echo $image_img_tag; ?></a></li>   
+		<?php endforeach ?>
+		 </ul>
+  
 
+<?php endif; ?> 
+</div> 
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
 				<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
