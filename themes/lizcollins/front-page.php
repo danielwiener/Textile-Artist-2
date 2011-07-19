a59775e31c5f2692731cafda13079ce7dfb72791
danielwiener <daniel@danielwiener.com>
2011-07-19 15:49:35 -0400

    formatting text overlay on front page. images and text is hardcoded in. no need to make it complicated/dynamic. will change by hand in the future. can change images by naming them the same as these.

--- themes/lizcollins/front-page.php
+++ themes/lizcollins/front-page.php
@@ -16,8 +16,8 @@ get_header(); ?>
 		<div id="container" class="full-page">
 			<div id="content" role="main">
 				<!-- <div id="front-page"> -->
-				     <div class="dw_wrapper"><a href="/lc/collections/some_collection" title="sometitle"><img src="<?php bloginfo('stylesheet_directory') ?>/images/collections.jpg" width="472" height="700" /></a><div class="dw_description"><div class="dw_description_content">Collections</div></div></div> 
-					 <div class="dw_wrapper"><a href="/lc/projects/knitting-nation/one" title="Knitting Nation One"><img src="<?php bloginfo('stylesheet_directory') ?>/images/projects.jpg"  width="472" height="700"></a><div class="dw_description"><div class="dw_description_content">Projects</div></div></div> 
+				     <div class="dw_wrapper"><a href="/lc/collections/some_collection" title="sometitle"><img src="<?php bloginfo('stylesheet_directory') ?>/images/collections.jpg" width="472" height="700" /></a><div class="dw_description"><div class="dw_description_content2"><a href="/lc/collections/some_collection" title="sometitle">Collections</a></div></div></div> 
+					 <div class="dw_wrapper"><a href="/lc/projects/knitting-nation/one" title="Knitting Nation One"><img src="<?php bloginfo('stylesheet_directory') ?>/images/projects.jpg"  width="472" height="700"></a><div class="dw_description"><div class="dw_description_content"><a href="/lc/projects/knitting-nation/one" title="Knitting Nation One">Projects</a></div></div></div> 
 				<!-- </div> -->   <br clear="both">
 			</div><!-- #content -->
 		</div><!-- #container -->
