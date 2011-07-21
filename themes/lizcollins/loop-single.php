21677c8a678e64c2225d05612512a3206183203f
danielwiener <daniel@danielwiener.com>
2011-07-21 14:23:23 -0400

    next and previous post links in the same category

--- themes/lizcollins/loop-single.php
+++ themes/lizcollins/loop-single.php
@@ -32,8 +32,8 @@
 				</div><!-- #post-## -->
 
 				<div id="nav-below" class="navigation">
-					<div class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'twentyten' ) . '</span> %title' ); ?></div>
-					<div class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'twentyten' ) . '</span>' ); ?></div>
+					<div class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'twentyten' ) . '</span> %title', TRUE ); ?></div>
+					<div class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'twentyten' ) . '</span>', TRUE ); ?></div>
 				</div><!-- #nav-below -->
 
 				<?php comments_template( '', true ); ?>
