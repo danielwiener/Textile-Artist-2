09e120098e46ff6e5bf8ba1bf06d5e49dd5697ca
danielwiener <daniel@danielwiener.com>
2011-07-21 12:36:02 -0400

    page for empty collections parent page to go to most recent Collection

---
+++ themes/lizcollins/content-recent_collections.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * The displays the content for the projects page it is here only to protect against someone accidentally going to the empty projects page.
+ *
+ * @package WordPress
+ * @subpackage Liz Collins
+ * @since Liz Collins 1.0
+ */
+?>  
+    
+<?php 	 
+		$collection_args = array (
+			'post_parent'		=> '93', //shows most recent Knitting Nation
+			'orderby'			=> 'date',
+			'order'				=> 'DESC',
+			'posts_per_page'		=> '1',
+			'post_type'			=> 'page',
+			'post_status'		=> 'published'
+			
+			);
+		$collection_query = new WP_Query( $collection_args );
+		  
+	      ?>
+
+<?php if ( $collection_query->have_posts() ) while ( $collection_query->have_posts() ) : $collection_query->the_post(); ?> 
+		<div id="image_grid">
+			<?php 	 
+				$image = '';
+				$args = array(
+					'post_parent' => $post->ID,
+					'post_type' => 'attachment',
+					'post_mime_type' => 'image',
+					'orderby' => 'menu_order',
+					'order' => 'ASC',
+					'numberposts' => -1,
+					'post_status' => 'inherit'
+				);
+				$images = get_children( $args );
+					if ( $images ) :
+						?><ul>
+							<?php foreach ($images as $image): ?>
+								<?php $image_img_tag = wp_get_attachment_image( $image->ID, 'thumbnail' );  
+								$img_url = wp_get_attachment_url($image->ID); // url of the full size image. ?> 
+						      	  <li><a href="<?php echo $img_url; ?>"><?php echo $image_img_tag; ?></a></li> 
+							<?php endforeach; ?>
+							</ul>
+					  <?php endif; ?> 
+		</div><br clear="both">
+
+				<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+ 
+						<h1 class="entry-title">
+						<?php if ($post->post_parent == 40): ?>
+						 Knitting Nation Phase    
+						<?php endif ?>
+						<?php the_title(); ?></h1>
+
+					<div class="entry-content">
+						<?php the_content(); ?>
+						<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'twentyten' ), 'after' => '</div>' ) ); ?>
+						<?php edit_post_link( __( 'Edit', 'twentyten' ), '<span class="edit-link">', '</span>' ); ?>
+					</div><!-- .entry-content -->
+				</div><!-- #post-## -->
+
+<?php endwhile; // end of the loop. ?>
--- themes/lizcollins/page-collections.php
+++ themes/lizcollins/page-collections.php
@@ -14,21 +14,14 @@
 
 get_header(); ?> 
 <?php
-	global $post;
-	if ($post->post_parent == 24 || $post->post_parent == 40) {	//check to see if it is in Projects or Knitting Nation
-		get_sidebar('projects');
-	} elseif ($post->post_parent == 93) { 						//check to see if it is in Collections
 		get_sidebar('collections');	
-	}   else {
-		get_sidebar(); 											//for all other pages, e.g. About
-	}
 ?> 
 
 		<div id="container">
 			<div id="content" role="main">
               
 			<?php
-				get_template_part( 'content', 'page' );
+				get_template_part( 'content', 'recent_collections' );
 			?>
 			</div><!-- #content -->
 		</div><!-- #container -->
