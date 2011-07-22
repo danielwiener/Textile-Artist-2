0ea38eb444dd8f5b5ce35cbfdd1fe7d9555b0299
danielwiener <daniel@danielwiener.com>
2011-07-22 10:30:10 -0400

    adding iframe and fancybox as classes to activate the fancybox plugin and adding rel = fancybox to thumbnail so all of them are in the same group.

--- themes/lizcollins/content-category_press.php
+++ themes/lizcollins/content-category_press.php
@@ -10,5 +10,5 @@
 
     <div id="image_grid"><ul>
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?> 		
-		<li><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a></li>
+		<li><a href="<?php the_permalink(); ?>" class="iframe fancybox"><?php the_post_thumbnail('thumbnail', array( 'rel'=>'fancybox') ); ?></a></li> 
 <?php endwhile; // end of the loop. ?>   </ul></div><br clear="both"> 
