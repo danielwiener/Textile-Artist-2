a7e8c85b30d167624544cf556e4e6d243c38c729
danielwiener <daniel@danielwiener.com>
2011-07-22 10:32:45 -0400

    added some comments for to-do's

--- themes/lizcollins/content-category_press.php
+++ themes/lizcollins/content-category_press.php
@@ -10,5 +10,5 @@
 
     <div id="image_grid"><ul>
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?> 		
-		<li><a href="<?php the_permalink(); ?>" class="iframe fancybox"><?php the_post_thumbnail('thumbnail', array( 'rel'=>'fancybox') ); ?></a></li> 
+		<li><a href="<?php the_permalink(); ?>" class="iframe fancybox"><?php the_post_thumbnail('thumbnail', array( 'rel'=>'fancybox') ); ?></a></li> <!-- if I were to do this for real, I would need to make a blank page with the google view link only -->
 <?php endwhile; // end of the loop. ?>   </ul></div><br clear="both"> 
