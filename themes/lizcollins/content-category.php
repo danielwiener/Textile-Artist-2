988ab8f3a7a582b928a4093ee42d5a537c4b690c
danielwiener <daniel@danielwiener.com>
2011-08-10 18:56:36 -0400

    if there are are more than 5 entries show as TOC page but if there are less show the each entire entry. now I don't have to go back and change it.

--- themes/lizcollins/content-category.php
+++ themes/lizcollins/content-category.php
@@ -45,7 +45,7 @@
 <?php while ( have_posts() ) : the_post(); ?>
            <?php 
 
-				if ($total_in_news > 1): ?>
+				if ($total_in_news > 5): ?>
            	 <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 					<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
 
