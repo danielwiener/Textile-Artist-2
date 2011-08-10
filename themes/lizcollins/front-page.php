06aeef9058f9666f4f78d637207e962c8c11030e
danielwiener <daniel@danielwiener.com>
2011-08-10 12:42:44 -0400

    changed urls for Collections and Knitting Nation

--- themes/lizcollins/front-page.php
+++ themes/lizcollins/front-page.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying all pages.
+ * The template for displaying the front page, divided between Projects and Collections.
  *
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages
@@ -16,8 +16,8 @@ get_header(); ?>
 		<div id="container" class="full-page">
 			<div id="content" role="main">
 				<!-- <div id="front-page"> -->
-				     <div class="dw_wrapper"><a href="<?php bloginfo('siteurl') ?>/collections/intro" title="Spring 2004"><img src="<?php bloginfo('stylesheet_directory') ?>/images/collections.jpg" width="472" height="700" /></a><div class="dw_description"><div class="dw_description_content2"><a href="/lc/collections/some_collection" title="sometitle">Collections</a></div></div></div> 
-					 <div class="dw_wrapper"><a href="<?php bloginfo('siteurl') ?>/projects/knitting-nation" title="Knitting Nation One"><img src="<?php bloginfo('stylesheet_directory') ?>/images/projects.jpg"  width="472" height="700"></a><div class="dw_description"><div class="dw_description_content"><a href="/lc/projects/knitting-nation/one" title="Knitting Nation One">Projects</a></div></div></div> 
+				     <div class="dw_wrapper"><a href="<?php bloginfo('siteurl') ?>/collections/intro" title="Collections"><img src="<?php bloginfo('stylesheet_directory') ?>/images/collections.jpg" width="472" height="700" /></a><div class="dw_description"><div class="dw_description_content2"><a href="/lc/collections/intro" title="Collections">Collections</a></div></div></div> 
+					 <div class="dw_wrapper"><a href="<?php bloginfo('siteurl') ?>/projects/knitting-nation" title="Knitting Nation"><img src="<?php bloginfo('stylesheet_directory') ?>/images/projects.jpg"  width="472" height="700"></a><div class="dw_description"><div class="dw_description_content"><a href="/lc/projects/knitting-nation" title="Knitting Nation">Projects</a></div></div></div> 
 				<!-- </div> -->   <br clear="both">
 			</div><!-- #content -->
 		</div><!-- #container -->
