f1a29cf2d81f6c36176fab31d5e981f9cec2b4c3
danielwiener <daniel@danielwiener.com>
2011-08-10 12:06:45 -0400

    adding templates to make a page for Knitting Nation Intro page with text and image and for Collections intro page too

---
+++ themes/lizcollins/page_text_image.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Liz Collins
+ * @since Liz Collins 1.0
+ */
+
+get_header(); ?> 
+<?php
+	global $post;
+	if ($post->post_parent == 24 || $post->post_parent == 40) {	//check to see if it is in Projects or Knitting Nation
+		get_sidebar('projects');
+	} elseif ($post->post_parent == 93) { 						//check to see if it is in Collections
+		get_sidebar('collections');	
+	}   else {
+		get_sidebar(); 											//for all other pages, e.g. About
+	}
+?> 
+
+		<div id="container">
+			<div id="content" role="main">
+              
+			<?php
+				get_template_part( 'content', 'page' );
+			?>
+			</div><!-- #content -->
+		</div><!-- #container -->
+
+
+<?php get_footer(); ?>  
