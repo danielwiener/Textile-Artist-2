6471f699569a6eed52f3f106254e7c3e535e2d42
danielwiener <daniel@danielwiener.com>
2011-08-10 14:28:52 -0400

    general Collections/Project sidebar added and correct formatting/layout added to 404 page

--- themes/lizcollins/404.php
+++ themes/lizcollins/404.php
@@ -7,16 +7,18 @@
  * @since Twenty Ten 1.0
  */
 
-get_header(); ?>
-
+get_header(); 
+   get_sidebar(); ?>
 	<div id="container">
 		<div id="content" role="main">
 
-			<div id="post-0" class="post error404 not-found">
+			<div id="post-0" class="post error404 not-found hentry">
 				<h1 class="entry-title"><?php _e( 'Not Found', 'twentyten' ); ?></h1>
 				<div class="entry-content">
 					<p><?php _e( 'Apologies, but the page you requested could not be found. Perhaps searching will help.', 'twentyten' ); ?></p>
-					<?php get_search_form(); ?>
+					<?php get_search_form(); ?> 
+					
+					
 				</div><!-- .entry-content -->
 			</div><!-- #post-0 -->
 
