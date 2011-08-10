5219c8e5067a376a8cf231596015184be23072f1
danielwiener <daniel@danielwiener.com>
2011-08-10 14:46:13 -0400

    formatting search.php put loop in the template, no need for loop-search, search won't be used that much

--- themes/lizcollins/search.php
+++ themes/lizcollins/search.php
@@ -8,19 +8,19 @@
  */
 
 get_header(); ?>
-
+  <?php get_sidebar(); ?> 
 		<div id="container">
 			<div id="content" role="main">
-<?php get_sidebar(); ?>
-<?php if ( have_posts() ) : ?>
+<?php if ( have_posts() ) : ?> 
+		<div id="post-<?php the_ID(); ?>" <?php post_class('hentry'); ?>> 
 				<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'twentyten' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-				<?php
-				/* Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called loop-search.php and that will be used instead.
-				 */
-				 get_template_part( 'loop', 'search' );
-				?>
+			   <?php while ( have_posts() ) : the_post(); ?> 
+				<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+			  	<div class="entry-summary">
+					<?php the_excerpt(); ?>
+				</div><!-- .entry-summary -->
+			   <?php endwhile; ?>
+			 
 <?php else : ?>
 				<div id="post-0" class="post no-results not-found hentry">
 					<h2 class="entry-title"><?php _e( 'Nothing Found', 'twentyten' ); ?></h2>
